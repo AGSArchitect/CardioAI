54a7c4a8896a75de2ea34a8c4ed49c61b3c53450
AGSArchitect <blackfossa@gmail.com>
2025-06-27 20:53:53 -0400

    CardioAI Payload Generator.

---
+++ PayloadGenerator/src/main/java/com/cardioai/tools/generator/PayloadGenerator.java
@@ -0,0 +1,13 @@
+package com.cardioai.tools.generator;
+
+/**
+ * PayloadGenerator
+ *
+ * @author Ariel Gonzalez
+ * @version 1.0
+ */
+public class PayloadGenerator {
+
+    public static void main(String[] args) {
+    }
+}
