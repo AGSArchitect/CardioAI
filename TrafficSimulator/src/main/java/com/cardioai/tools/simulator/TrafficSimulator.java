261c667666975ea03792fe454d0e71b0ad61c966
AGSArchitect <blackfossa@gmail.com>
2025-06-30 15:15:07 -0400

    Initial configuration.

---
+++ TrafficSimulator/src/main/java/com/cardioai/tools/simulator/TrafficSimulator.java
@@ -0,0 +1,14 @@
+package com.cardioai.tools.simulator;
+
+
+/**
+ * TrafficSimulator
+ *
+ * @author Ariel Gonzalez
+ * @version 1.0
+ */
+public class TrafficSimulator {
+
+    public static void main(String[] args) {
+    }
+}
