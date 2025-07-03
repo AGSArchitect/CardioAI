5486662c0d7ec01a75866c07c9ef905d599d25a5
AGSArchitect <blackfossa@gmail.com>
2025-07-03 14:00:43 -0400

    Added a description to the log messages.

--- DataExtractor/src/main/java/com/cardioai/tools/extractor/LogMessage.java
+++ DataExtractor/src/main/java/com/cardioai/tools/extractor/LogMessage.java
@@ -8,19 +8,19 @@ package com.cardioai.tools.extractor;
  */
 public enum LogMessage {
     /**
-     * DE001
+     * DE001. The manifest value is either not a file or does not exist.
      */
     DE001("code=\"{}\" message=\"The manifest value is either not a file or does not exist\" file=\"{}\""),
     /**
-     * DE002
+     * DE002. The destination value is either not a directory or does not exist.
      */
     DE002("code=\"{}\" message=\"The destination value is either not a directory or does not exist\" directory=\"{}\""),
     /**
-     * DE003
+     * DE003. The command line argument is unknown.
      */
     DE003("code=\"{}\" message=\"The command line argument is unknown\" argument=\"{}\""),
     /**
-     * DE004
+     * DE004. The header value is either not a file or does not exist.
      */
     DE004("code=\"{}\" message=\"The header value is either not a file or does not exist\" file=\"{}\"");
 
