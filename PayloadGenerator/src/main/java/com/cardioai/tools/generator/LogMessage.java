0f20f1687c6e4f76ed59054ddd65bc0b047a0f2c
AGSArchitect <blackfossa@gmail.com>
2025-07-03 14:00:34 -0400

    Added a description to the log messages.

--- PayloadGenerator/src/main/java/com/cardioai/tools/generator/LogMessage.java
+++ PayloadGenerator/src/main/java/com/cardioai/tools/generator/LogMessage.java
@@ -8,15 +8,15 @@ package com.cardioai.tools.generator;
  */
 public enum LogMessage {
     /**
-     * PG001
+     * PG001. The source value is either not a directory or does not exist.
      */
     PG001("code=\"{}\" message=\"The source value is either not a directory or does not exist\" directory=\"{}\""),
     /**
-     * PG002
+     * PG002. The destination value is either not a directory or does not exist.
      */
     PG002("code=\"{}\" message=\"The destination value is either not a directory or does not exist\" directory=\"{}\""),
     /**
-     * PG003
+     * PG003. The command line argument is unknown.
      */
     PG003("code=\"{}\" message=\"The command line argument is unknown\" argument=\"{}\"");
 
