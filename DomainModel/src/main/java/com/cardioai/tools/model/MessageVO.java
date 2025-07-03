12ca294c09fc8601bec6772d8da49a7b4c8c01b6
AGSArchitect <blackfossa@gmail.com>
2025-07-03 03:05:35 -0400

    Made the MessageVO immutable.

--- DomainModel/src/main/java/com/cardioai/tools/model/MessageVO.java
+++ DomainModel/src/main/java/com/cardioai/tools/model/MessageVO.java
@@ -8,58 +8,55 @@ package com.cardioai.tools.model;
  */
 public class MessageVO {
 
-    private String originId;
-    private String messageId;
-    private String messageType;
-    private int messageVersion;
-    private PayloadVO payload;
-    private long created;
+    private final String originId;
+    private final String messageId;
+    private final String originCode;
+    private final String deviceCode;
+    private final String messageType;
+    private final int messageVersion;
+    private final PayloadVO payload;
+    private final long created;
+
+    public MessageVO(String originId, String messageId, String originCode, String deviceCode, String messageType, int messageVersion, PayloadVO payload, long created) {
+        this.originId = originId;
+        this.messageId = messageId;
+        this.originCode = originCode;
+        this.deviceCode = deviceCode;
+        this.messageType = messageType;
+        this.messageVersion = messageVersion;
+        this.payload = payload;
+        this.created = created;
+    }
 
     public String getOriginId() {
         return originId;
-    }
-
-    public void setOriginId(String originId) {
-        this.originId = originId;
     }
 
     public String getMessageId() {
         return messageId;
     }
 
-    public void setMessageId(String messageId) {
-        this.messageId = messageId;
+    public String getOriginCode() {
+        return originCode;
+    }
+
+    public String getDeviceCode() {
+        return deviceCode;
     }
 
     public String getMessageType() {
         return messageType;
     }
 
-    public void setMessageType(String messageType) {
-        this.messageType = messageType;
-    }
-
     public int getMessageVersion() {
         return messageVersion;
-    }
-
-    public void setMessageVersion(int messageVersion) {
-        this.messageVersion = messageVersion;
     }
 
     public PayloadVO getPayload() {
         return payload;
     }
 
-    public void setPayload(PayloadVO payload) {
-        this.payload = payload;
-    }
-
     public long getCreated() {
         return created;
-    }
-
-    public void setCreated(long created) {
-        this.created = created;
     }
 }
